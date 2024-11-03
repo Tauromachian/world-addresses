ef9dc3fbbd55b7ac50e614c214b5f070f2b49cc9
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-11-02 20:47:20 -0400

    Install and use TypeORM

--- config/db.ts
+++ config/db.ts
@@ -1,11 +1,22 @@
+import 'reflect-metadata';
 import { FastifyInstance } from 'fastify';
 
-import postgres from '@fastify/postgres';
+import dbConnection from 'typeorm-fastify-plugin';
 
 import 'dotenv/config';
 
+import { Country } from '../entities/country';
+
 export async function registerDb(fastify: FastifyInstance) {
-  fastify.register(postgres, {
-    connectionString: process.env.DB_POSTGRES_STRING,
+  fastify.register(dbConnection, {
+    type: (process?.env?.DB_TYPE as 'mysql' | 'mariadb' | 'postgres' | 'sqlite' | undefined) ?? 'postgres',
+    host: process?.env?.DB_HOST ?? 'localhost',
+    port: Number(process?.env?.DB_PORT) || 5432,
+    username: process.env.DB_USER ?? 'postgres',
+    password: process.env.DB_PASSWORD ?? '123',
+    database: process.env.DB_DATABASE ?? 'world',
+    synchronize: true,
+    logging: false,
+    entities: [Country],
   });
 }
