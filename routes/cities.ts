d2bbe190ed65240bfd063ecb4ef4150358ff22e2
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-11-04 08:03:04 -0500

    Add cities basic endpoint

---
+++ routes/cities.ts
@@ -0,0 +1,20 @@
+import { FastifyInstance } from 'fastify';
+import { City } from '../entities/city';
+
+export function registerCountryApi(fastify: FastifyInstance) {
+  fastify.get('/city', async function () {
+    const cityRepository = fastify.orm.getRepository(City);
+
+    try {
+      const cities = await cityRepository.find();
+
+      return cities;
+    } catch (error) {
+      if (error instanceof Error) {
+        console.warn(error.message);
+        return;
+      }
+      console.warn(error);
+    }
+  });
+}
